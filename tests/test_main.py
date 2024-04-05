9693449fd8467e2d3ec9c8b39b5d36bf0f60b173
VolodymyrKhodatskyi <datomir777@gmail.com>
2024-04-05 14:22:59 +0300

    added test_sort_area

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,3 +5,14 @@ def test_read_txt():
     data = main.read_txt("testtext.txt")
     return data
 
+def test_sort_area(test_read_txt):
+    data = test_read_txt
+    sorted_data = main.sort_area(data)
+    expected_data = [('Франція', 100000.0, 80000),
+                     ('Англія', 200000.0, 15000),
+                     ('Україна', 600000.0, 42000),
+                     ('Німечинна', 800000.0, 50000),
+                     ('США', 900000.0, 17000),
+                     ('Китай', 1000000.0, 900000)]
+    assert sorted_data == expected_data
+
