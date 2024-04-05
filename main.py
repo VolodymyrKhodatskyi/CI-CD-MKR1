0aa8ad04d05ea3281f385724ca6074276c992036
VolodymyrKhodatskyi <datomir777@gmail.com>
2024-04-05 14:20:45 +0300

    added function read_txt

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+def read_txt(file_name):
+    data = []
+    with open(file_name, 'r', encoding='utf-8') as file:
+        for line in file:
+            country, area, population = line.strip().split(',')
+            data.append((country, float(area), int(population)))
+    return data
+
+
 
