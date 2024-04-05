927a5484e4a721b6f4bdb564db2ecbf0affb214d
VolodymyrKhodatskyi <datomir777@gmail.com>
2024-04-05 14:23:13 +0300

    added test_sort_population

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,3 +16,13 @@ def test_sort_area(test_read_txt):
                      ('Китай', 1000000.0, 900000)]
     assert sorted_data == expected_data
 
+def test_sort_population(test_read_txt):
+    data = test_read_txt
+    sorted_data = main.sort_population(data)
+    expected_data = [('Англія', 200000.0, 15000),
+                     ('США', 900000.0, 17000),
+                     ('Україна', 600000.0, 42000),
+                     ('Німечинна', 800000.0, 50000),
+                     ('Франція', 100000.0, 80000),
+                     ('Китай', 1000000.0, 900000)]
+    assert sorted_data == expected_data
